d9a162686cfde83b304ff1702ddf10e2366c414f
ryanowalls <rowalls@me.com>
2024-08-12 15:18:02 +0100

    Fix

--- app/immutable/entry/start.C-O1OCC-.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DggpJQ28.js";export{t as start};
--- banner-ads/screwfix/world_cup_300x250/config.js
+++
@@ -1,3 +0,0 @@
-var defaultClickURL = [
-          {productLabel:"homepage6",productURL:"https://www.screwfix.com/legendsofthegame"}
-           ];
